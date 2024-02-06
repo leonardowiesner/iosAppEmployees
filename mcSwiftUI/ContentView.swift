6683784096ae815989c88a8eb914b78012166c82
leonardowiesner <leonardowiesner11@gmail.com>
2024-02-06 23:20:49 +0100

    Initial Commit

---
+++ mcSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mcSwiftUI
+//
+//  Created by Leonardo Wiesner on 6/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mcSwiftUI/mcSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  mcSwiftUIApp.swift
+//  mcSwiftUI
+//
+//  Created by Leonardo Wiesner on 6/2/24.
+//
+
+import SwiftUI
+
+@main
+struct mcSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
